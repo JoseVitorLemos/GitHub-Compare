13b39578b336e2a845751c94c1b8778482b76e1b
José Vitor <jose81073175@gmail.com>
2020-04-12 02:03:34 -0300

    componentList & axios...

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../assets/logo.png'
 import { Container } from '../components/container'
 import { Form } from '../components/form'
 import CompareList from '../components/ComperList'
+import api from '../services/api'
 
-const Main = () => (
-  <Container>
-    <img src={Logo} alt="Github Compare" />
-    <Form>
-      <input type="text" placeholder="usuário/repositório" />
-      <button type="submit">OK</button>
-    </Form>
+const Main = () => {
+  const [state, setState] = useState({
+    repositoryInput: '',
+    repositories: [],
+    mapear: false
+  })
 
-    <CompareList />
-  </Container>
+  console.log(state.repositoryInput)
+  console.log(state.mapear)
 
-)
+  const handleAddRepository = async (event) => {
+    event.preventDefault()
+
+    try {
+      const response = await api.get(`/repos/${state.repositoryInput}`)
+      setState({ mapear: true })
+      console.log(response)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <Container>
+      <img src={Logo} alt="Github Compare" />
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          placeholder="usuário/repositório"
+          value={state.repositoryInput}
+          onChange={event => setState({ repositoryInput: event.target.value })}
+        />
+        <button type="submit">OK</button>
+      </Form>
+      <CompareList repositories={state.repositories} mapear={state.mapear} />
+    </Container>
+  )
+}
 
 export default Main
