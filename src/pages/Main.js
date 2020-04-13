ca85aa9e15cf3eef7b8ea3092182b13aaec3f9ed
José Vitor <jose81073175@gmail.com>
2020-04-12 22:23:59 -0300

    Ultimo commit do projeto. Adicionando spinner com font-awesome e estilizações globais

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import moment from 'moment'
 import api from '../services/api'
 import Logo from '../assets/logo.png'
 import { Container } from '../components/container'
@@ -9,19 +10,29 @@ export default class Main extends Component {
   state = {
     repositoryInput: '',
     repositories: [],
+    repositoryError: false,
+    isLoading: false
   }
 
   handleAddRepository = async (event) => {
     event.preventDefault()
 
+    this.setState({ isLoading: true })
+
     try {
-      const response = await api.get(`/repos/${this.state.repositoryInput}`)
+      const { data: repository } = await api.get(`/repos/${this.state.repositoryInput}`)
+
+      repository.lastCommit = moment(repository.pushed_at).fromNow() //Atribuiu uma variável chamada lastCommit recebendo uma data formatada usando a biblioteca momment
+
       this.setState({
         repositoryInput: '',
-        repositories: [...this.state.repositories, response.data]
+        repositories: [...this.state.repositories, repository],
+        repositoryError: false
       })
     } catch (err) {
-      console.log(err)
+      this.setState({ repositoryError: true })
+    } finally {
+      this.setState({ isLoading: false })
     }
   }
 
@@ -30,14 +41,14 @@ export default class Main extends Component {
       <Container>
         <img src={Logo} alt="Github Compare" />
 
-        <Form onSubmit={this.handleAddRepository}>
+        <Form withError={this.state.repositoryError} onSubmit={this.handleAddRepository}>
           <input
             type="text"
             placeholder="usuário/repositório"
             value={this.state.repositoryInput}
             onChange={event => this.setState({ repositoryInput: event.target.value })}
           />
-          <button type="submit">OK</button>
+          <button type="submit">{this.state.isLoading ? <i className="fa fa-spinner fa-pulse" /> : 'OK'}</button>
         </Form>
 
         <CompareList repositories={this.state.repositories} />
